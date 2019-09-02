d06811766366771e26057b05398a775f062f59fb
Jenia Moroz <eugen.v.moroz@gmail.com>
2019-09-02 21:04:22 +0200

    initial project setup

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/index.scss';
+
+const Index = () => (
+  <div className="root">
+    <p>Welcome</p>
+    <img src="/static/logo_1.svg" alt="UXI Sasha Suslova" />
+  </div>
+);
+
+export default Index;
